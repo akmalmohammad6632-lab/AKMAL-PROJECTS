d68dcef7e3f46e4cbeee6d6bb6e0137a6b2f5644
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-17 17:00:20 +0530

    Add files via upload

---
+++ bitwise operator.py
@@ -0,0 +1,16 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=5
+>>> b=3
+>>> print("AND:",a&b)
+AND: 1
+>>> print("OR:",a|b)
+OR: 7
+>>> print("XOR:",a^b)
+XOR: 6
+>>> print("NOT:",~a)
+NOT: -6
+>>> print("Left Shift:",a<<b)
+Left Shift: 40
+>>> print("Right shift:",a>>b)
+Right shift: 0
