deee425ff438b77b70cf8a8d0e58bd75fef6fbe2
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-17 16:48:42 +0530

    Add files via upload

---
+++ membership operator.py
@@ -0,0 +1,11 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=[10,20,30,40,50]
+>>> print(30 in a)
+True
+>>> print(60 in a)
+False
+>>> print(60 not in a)
+True
+>>> print(30 not in a)
+False
