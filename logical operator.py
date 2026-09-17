749cc220bde2328246a63e38924ca69600e98b23
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-17 14:49:49 +0530

    Add files via upload

---
+++ logical operator.py
@@ -0,0 +1,16 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=8
+>>> b=10
+>>> a<b and b>a
+True
+>>> a<=b and b>=a
+True
+>>> a!=b and b==a
+False
+>>> a<b or b>a
+True
+>>> a<=b or b>=a
+True
+>>> a!=b or b==a
+True
