2d98c460c3ea94683cc0a85ce2408613a7038a6f
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-17 16:33:36 +0530

    Add files via upload

---
+++ identify operator.py
@@ -0,0 +1,11 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=5
+>>> type(a)is int
+True
+>>> type(a) is not int
+False
+>>> type(a)is float
+False
+>>> type(a)is not float
+True
