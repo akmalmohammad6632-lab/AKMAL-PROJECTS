9cf405b17ac4e8cec6b2ae3318a587a30f943c98
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-17 10:22:54 +0530

    Add files via upload

---
+++ arthametic operator.py
@@ -0,0 +1,16 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=3
+>>> b=6
+>>> print(a+b)
+9
+>>> print(a-b)
+-3
+>>> print(a*b)
+18
+>>> print(a//b)
+0
+>>> print(a/b)
+0.5
+>>> print(a**b)
+729
