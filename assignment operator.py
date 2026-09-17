d52164ffd44ab4368c8cf11d467efdec9f299608
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-17 10:29:31 +0530

    Add files via upload

---
+++ assignment operator.py
@@ -0,0 +1,22 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=3
+>>> b=4
+>>> a+=b
+>>> a
+7
+>>> a-=b
+>>> a
+3
+>>> a//=2
+>>> a
+1
+>>> a/=3
+>>> a
+0.3333333333333333
+>>> a**=2
+>>> a
+0.1111111111111111
+>>> a%=4
+>>> a
+0.1111111111111111
