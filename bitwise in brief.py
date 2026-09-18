a791d0ef8aa58d6669a19c8695b464579a8e538b
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-18 18:53:32 +0530

    Add files via upload

---
+++ bitwise in brief.py
@@ -0,0 +1,22 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #bitwise operations
+>>> #& , | , ~ , ^ , << ,>>
+>>> a=3
+>>> b=5
+>>> a&b
+1
+>>> bin(3)
+'0b11'
+>>> bin(5)
+'0b101'
+>>> #bitwise operations
+>>> #& , | , ~ , ^ , << , >>
+>>> a=2
+>>> b=5
+>>> bin(2)
+'0b10'
+>>> bin(4)
+'0b100'
+>>> a&b
+0
