5b4cea29eb423ebd924d85cc745b3d510d21089a
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-18 19:25:16 +0530

    Add files via upload

---
+++ XOR operator EX.py
@@ -0,0 +1,12 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=4
+>>> b=7
+>>> bin(4)
+'0b100'
+>>> bin(7)
+'0b111'
+>>> a^b
+3
+>>> 
+>>> 
