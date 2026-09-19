87803b40ac1121206663d27ee3dad61ddec62375
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-19 20:57:33 +0530

    Add files via upload

---
+++ negetive indexing.py
@@ -0,0 +1,24 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #negetive indexing
+>>> a="time is precious"
+>>> a[-8]+a[-7]+a[-6]+a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'precious'
+>>> a[-11]+a[-10]
+'is'
+>>> a[-16]+a[-15]+a[-14]+a[-13]
+'time'
+>>> 
+...  
+>>> 
+>>> a="vijayavada is a royal city"
+>>> a[-4]+a[-3]+a[-2]+a[-1]
+'city'
+>>> a[-12]
+'a'
+>>> a[-10]+a[-9]+a[-8]+a[-7]+a[-6]
+'royal'
+>>> a[-15]+a[-14]
+'is'
+>>> a[-26]+a[-25]+a[-24]+a[-23]+a[-22]+a[-21]+a[-20]+a[-19]+a[-18]+a[-17]
+'vijayavada'
