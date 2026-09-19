ee18d628df9e5ea88ac070fd4e6e0a31fb985f41
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-19 21:03:23 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,36 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #slicing
+>>> a="codegnan"
+>>> a[0:4]
+'code'
+>>> a[4:8]
+'gnan'
+>>> a[0:8]
+'codegnan'
+>>> a[:4]
+'code'
+>>> a[4:]
+'gnan'
+>>> a[0:8]
+'codegnan'
+>>> a="mallareddy"
+>>> a[0:5]
+'malla'
+>>> a[5:10]
+'reddy'
+>>> a[0:10]
+'mallareddy'
+>>> a[:4]
+'mall'
+>>> a[:5]
+'malla'
+>>> a[:10]
+'mallareddy'
+>>> a="india"
+>>> a[0:2]
+'in'
+>>> a[2:6]
+'dia'
+>>> a[0:6]
+'india'
