3f7f97f457e0ae83df31c13d01d31dec908229fb
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-19 20:47:41 +0530

    Add files via upload

---
+++ positive indexing.py
@@ -0,0 +1,44 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #indexing
+>>> a="i am in class"
+>>> a[8]+a[9]+a[10]+a[11]+a[12]
+'class'
+>>> a[5]+a[6]
+'in'
+>>> a[2]+a[3]
+'am'
+>>> a[0]
+'i'
+>>> a[1]
+' '
+>>> a[4]
+' '
+>>> a[7]
+' '
+>>> a[1]
+' '
+>>> a[1]+a[4]+a[7]
+'   '
+>>> 
+...   
+>>> 
+>>> a="i am a software engeneer"
+>>> a[16]+a[17]+a[18]+a[19]+a[20]+a[21]+a[22]+a[23]
+'engeneer'
+>>> a[7]+a[8]+a[9]+a[10]+a[11]+a[12]+a[13]+a[14]
+'software'
+>>> a[5]
+'a'
+>>> a[2]+a[3]
+'am'
+>>> a[0]
+'i'
+>>> a[1]
+' '
+>>> a[4]
+' '
+>>> a[6]
+' '
+>>> a[1]+a[4]+a[6]
+'   '
