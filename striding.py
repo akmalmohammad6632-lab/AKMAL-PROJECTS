b9440aa2369b51c9994afae3c9147a014f137087
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-22 21:44:39 +0530

    Add files via upload

---
+++ striding.py
@@ -0,0 +1,34 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#striding
+a="data science"
+>>> a[::]
+'data science'
+>>> a[::1]
+'data science'
+>>> a[::3]
+'dacn'
+>>> a="machine learnng"
+>>> a[::2]
+'mcielang'
+>>> a[::4]
+'miln'
+>>> a[5:9]
+'ne l'
+>>> a[6:]
+'e learnng'
+>>> a[:11]
+'machine lea'
+>>> a[::8]
+'ml'
+>>> a="cloud computing"
+>>> a[1:8:2]
+'lu o'
+>>> a[2:12:4]
+'ocu'
+>>> a[1:13:3]
+'ldou'
+>>> a[1:14:5]
+'lct'
+>>> a[3:9:4]
+'uo'
