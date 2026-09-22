308eecb564fa63a513717d80669746a70673c159
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-22 21:38:06 +0530

    Add files via upload

---
+++ negetive sliciing.py
@@ -0,0 +1,22 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #negetive slicing
+>>> a="codegnan it solutions"
+>>> a[-17:-3]
+'gnan it soluti'
+>>> a[-17:-13]
+'gnan'
+>>> a[-12:-10]
+'it'
+>>> a[-9:-1]
+'solution'
+>>> b="vijayawada is a royal city"
+>>> b[-10:-5]
+'royal'
+>>> b[-26:-14]
+'vijayawada i'
+>>> b[-4]:
+...     
+SyntaxError: invalid syntax
+>>> b[-4]
+'c'
