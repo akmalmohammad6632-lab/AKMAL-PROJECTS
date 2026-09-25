e879b7e0d6c667d458d6506bda8f014f7a620663
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-25 11:00:20 +0530

    Add files via upload

---
+++ neegtive striding.py
@@ -0,0 +1,16 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #negetive striding"
+>>> a="python course"
+>>> a[-1:-8:-2]
+'ero '
+>>> a[-2:-12:-3]
+'sont'
+>>> a[-4:-13:-4]
+'uny'
+>>> a[-8:-4:2]
+'nc'
+>>> a[4:8:2]
+'o '
+>>> a[4:-8:-2]
+''
