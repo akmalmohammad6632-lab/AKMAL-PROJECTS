063f012b4c3d02003562e6faf975d2cdb3b7ae6c
akmalmohammad6632-lab <akmalmohammad6632@gmail.com>
2026-09-25 11:04:25 +0530

    Add files via upload

---
+++ length.py
@@ -0,0 +1,22 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #string methods
+>>> #len()
+>>> a="akmal"
+>>> len(a)
+5
+>>> b="hyderabad is a city"
+>>> len(b)
+19
+>>> c=""
+>>> len(c)
+0
+>>> d=" "
+>>> len(d)
+1
+>>> e="huzurabad"
+>>> len(e)
+9
+>>> a="123456789"
+>>> len(a)
+9
